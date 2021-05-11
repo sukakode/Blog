f6d23185d267b12ae2f2bb79ef1bf118da0bcb44
SukaKode <gsukakode@gmail.com>
2021-05-12 00:12:40 +0700

    route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+
+$frontend = 'App\Http\Controllers\Frontend\\';
 
 /*
 |--------------------------------------------------------------------------
@@ -13,9 +16,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', [$frontend . MainController::class, 'index']);
 
 Auth::routes();
 
